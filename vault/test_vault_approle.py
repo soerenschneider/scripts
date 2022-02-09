995e9c882b942a8022e2f61f242150bd31668e90
soeren <56670304+soerenschneider@users.noreply.github.com>
2022-02-09 18:58:44 +0100

    refactor: rename file

--- vault/test_vault_approle.py
+++
@@ -1,7 +0,0 @@
-from unittest import TestCase
-
-from vault_approle import Utils
-
-class TestUtils(TestCase):
-    def test_parse_datetime(self):
-        Utils.parse_datetime("2022-01-16T17:45:16.7255+01:00")
