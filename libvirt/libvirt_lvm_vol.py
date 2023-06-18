c1a5707a736e7aafb6028fc80080fa613a739c0c
soeren <56670304+soerenschneider@users.noreply.github.com>
2023-06-18 12:43:38 +0200

    refactor: change interface

--- libvirt/libvirt_lvm_vol.py
+++ libvirt/libvirt_lvm_vol.py
@@ -67,16 +67,23 @@ def parse_args() -> argparse.Namespace:
 
 class Calls(ABC):
     @abstractmethod
+    def vg_exists(self, vg_name: str) -> bool:
+        pass
+
+    @abstractmethod
     def volume_exists(self, vg_name: str, vol_name: str) -> bool:
         pass
 
     @abstractmethod
-    def create_volume(self, vg_name: str, vol_name: str, base_image: Path, vm_size_g: int) -> None:
+    def create_volume(self, vg_name: str, vol_name: str, base_image: Path, vol_size: int) -> None:
         pass
 
     @abstractmethod
     def remove_volume(self, vg_name: str, volume_name: str):
         pass
+
+    def is_domain_running(self, vm_name: str) -> bool:
+        return False
 
     @abstractmethod
     def shutdown_domain(self, domain_name: str):
@@ -88,11 +95,14 @@ class Calls(ABC):
 
 
 class NoopCalls(Calls):
+    def vg_exists(self, vg_name: str) -> bool:
+        return True
+
     def volume_exists(self, vg_name: str, vol_name: str) -> bool:
         return False
 
-    def create_volume(self, vg_name: str, vol_name: str, base_image: Path, vm_size_g: int) -> None:
-        logging.info("create volume for %s/%s using %s (%dGiB)", vg_name, vol_name, base_image, vm_size_g)
+    def create_volume(self, vg_name: str, vol_name: str, base_image: Path, vol_size: int) -> None:
+        logging.info("create volume for %s/%s using %s (%dGiB)", vg_name, vol_name, base_image, vol_size)
 
     def remove_volume(self, vg_name: str, volume_name: str):
         print(f"remove volume for %s/%s", vg_name, volume_name)
@@ -103,35 +113,58 @@ class NoopCalls(Calls):
     def start_domain(self, domain_name):
         print(f"start domain %s", domain_name)
 
+    def is_domain_running(self, vm_name: str) -> bool:
+        return False
+
 
 class NativeBinaries(Calls):
+    def vg_exists(self, vg_name: str) -> bool:
+        try:
+            subprocess.run(["lvdisplay", "-C", "--select", f"vg_name={vg_name}"], capture_output=True, check=True)
+            return True
+        except subprocess.CalledProcessError:
+            return False
+
     def volume_exists(self, vg_name: str, vol_name: str) -> bool:
-        output = subprocess.run(["lvdisplay", "-C", "--select", f"vg_name={vg_name}"], capture_output=True, text=True).stdout
+        if not self.vg_exists(vg_name):
+            return False
+
+        result = subprocess.run(["lvdisplay", "-C", "--select", f"vg_name={vg_name}"], capture_output=True, text=True, check=True)
+        output = result.stdout
         existing_vms = [line.split()[0] for line in output.splitlines()[1:]]
         return vol_name in existing_vms
 
-    def create_volume(self, vg_name: str, vol_name: str, base_image: Path, vm_size_g: int = None):
-        if not vm_size_g:
-            vm_size_g = 30
+    def create_volume(self, vg_name: str, vol_name: str, base_image: Path, vol_size: int = None):
+        if not vol_size:
+            vol_size = default_vol_size_g
 
-        subprocess.run(["lvcreate", "-L", f"{vm_size_g}G", "-n", vol_name, vg_name])
+        subprocess.run(["lvcreate", "-L", f"{vol_size}G", "-n", vol_name, vg_name], check=True)
         dst = f"/dev/mapper/{vg_name}-{vol_name}"
-        subprocess.run(["qemu-img", "convert", base_image, "-O", "raw", dst])
-        subprocess.run(["lvresize", "-L", f"{vm_size_g}G", f"{vg_name}/{vol_name}"])
+        subprocess.run(["qemu-img", "convert", base_image, "-O", "raw", dst], check=True)
+        #subprocess.run(["lvresize", "-L", f"{vol_size}G", f"{vg_name}/{vol_name}"], check=True)
 
     def remove_volume(self, vg_name: str, volume_name: str):
         lv_name = f"{vg_name}/{volume_name}"
         command = ["lvremove", "-f", lv_name]
         subprocess.run(command, check=True)
 
+    def is_domain_running(self, vm_name: str) -> bool:
+        try:
+            result = subprocess.run(['virsh', 'list', '--name'], capture_output=True, text=True, check=True)
+            output = result.stdout
+            return vm_name in output.splitlines()
+        except subprocess.CalledProcessError as e:
+            logging.error("Error talking to libvirt", e)
+            return False
+
     def shutdown_domain(self, domain_name: str):
         # todo: check if actually running and add error handling
-        command = ["virsh", "shutdown", domain_name]
+        command = ["virsh", "destroy", domain_name]
         subprocess.run(command, check=True)
 
     def start_domain(self, domain_name: str):
         command = ["virsh", "start", domain_name]
-        subprocess.run(command, check=True)
+        subprocess.run(command)
 
 
 def find_baseimage(base_dir: str, file_name: str) -> Optional[str]:
@@ -170,11 +203,10 @@ def _hostname_without_domain(hostname: str) -> str:
     return hostname
 
 
-def iterate_vms(datacenter: str, vm_host: str, hosts_data: Dict[str, any], args: argparse.Namespace) -> None:
+def iterate_vms(datacenter: str, vm_host: str, hosts_data: Dict[str, any], args: argparse.Namespace, impl: Calls) -> None:
     if datacenter not in hosts_data['local_hosts']:
         return
 
-    impl = NoopCalls() if args.dry_run else None
     simple_hostname = _hostname_without_domain(vm_host)
     for host in hosts_data["local_hosts"][datacenter]:
         if "vm_config" not in host or host["vm_config"]["host"] not in [simple_hostname, vm_host]:
@@ -185,7 +217,7 @@ def iterate_vms(datacenter: str, vm_host: str, hosts_data: Dict[str, any], args:
         wanted_os = host["vm_config"]["os"]
         base_image = find_baseimage(args.base_image_dir, wanted_os)
         if not base_image:
-            logging.error("could not find any images for '%s' in dir '%s'", os, args.base_image_dir)
+            logging.error("could not find any images for '%s' in dir '%s'", wanted_os, args.base_image_dir)
             continue
 
         create_volume(vg_name=args.vg_name, vol_name=vm_name, base_image=base_image, vol_size=disk_size, domain_name=vm_name, force_recreate=args.force_recreate, impl=impl)
@@ -210,9 +242,7 @@ def _get_hosts_data(hosts_file: str) -> Dict[str, any]:
         return yaml.safe_load(file)
 
 
-def create_volume(vg_name: str, vol_name: str, base_image: str, vol_size: int = None, domain_name: str = None, force_recreate: bool = False, impl: Calls = None):
-    if not impl:
-        impl = NoopCalls()
+def create_volume(vg_name: str, vol_name: str, base_image: str, impl: Calls, vol_size: int = None, domain_name: str = None, force_recreate: bool = False):
     if not domain_name:
         domain_name = vol_name
     if not vol_size:
@@ -225,17 +255,31 @@ def create_volume(vg_name: str, vol_name: str, base_image: str, vol_size: int = 
             logging.error("Not forcing re-creation of volume, exiting.")
             return
 
-        impl.shutdown_domain(domain_name)
-        impl.remove_volume(vg_name=vg_name, volume_name=vol_name)
-        impl.start_domain(domain_name)
+        power_cycle_domain = impl.is_domain_running(domain_name)
+        if power_cycle_domain:
+            impl.shutdown_domain(domain_name)
 
-    impl.create_volume(vg_name=vg_name, vol_name=vol_name, base_image=base_image, vm_size_g=vol_size)
+        impl.remove_volume(vg_name=vg_name, volume_name=vol_name)
+
+        if power_cycle_domain:
+            impl.start_domain(domain_name)
+
+    try:
+        impl.create_volume(vg_name=vg_name, vol_name=vol_name, base_image=base_image, vol_size=vol_size)
+    except subprocess.CalledProcessError as e:
+        logging.error("creating volume failed: %s", e)
 
 
 def main():
-    logging.basicConfig(format='%(levelname)s %(message)s')
+    logging.basicConfig(format='%(levelname)-8s %(message)s')
     logging.getLogger().setLevel(logging.INFO)
     args = parse_args()
+
+    impl = NoopCalls() if args.dry_run else NativeBinaries()
+
+    if not impl.vg_exists(args.vg_name):
+        logging.error("volume group '%s' does not exist", args.vg_name)
+        sys.exit(1)
 
     if args.subcommand == subcommands["sync"]:
         vm_host = args.vm_host if args.vm_host else socket.gethostname()
@@ -247,12 +291,10 @@ def main():
             logging.info("Detected datacenter '%s' from hostname '%s'", datacenter, vm_host)
 
         hosts_data = _get_hosts_data(args.hosts_file)
-        logging.info("Loaded hosts_data with %d entries for this dc", len(hosts_data["local_hosts"][datacenter]))
-        iterate_vms(datacenter=datacenter, vm_host=vm_host, hosts_data=hosts_data, args=args)
+        logging.info("Loaded hosts_data with %d entries for dc %s", len(hosts_data["local_hosts"][datacenter]), datacenter)
+        iterate_vms(datacenter=datacenter, vm_host=vm_host, hosts_data=hosts_data, args=args, impl=impl)
     elif args.subcommand == subcommands["create"]:
-        if args.dry_run:
-            impl = NoopCalls()
-        create_volume(vg_name=args.vg_name, vol_name=args.vol_name, base_image=args.base_image, vol_size=args.vol_size, domain_name=args.domain_name, force_recreate=args.force_recreate, impl=impl)
+        create_volume(vg_name=args.vg_name, vol_name=args.vol_name, base_image=args.base_image, impl=impl, vol_size=args.vol_size, domain_name=args.domain_name, force_recreate=args.force_recreate)
 
 
 if __name__ == "__main__":
